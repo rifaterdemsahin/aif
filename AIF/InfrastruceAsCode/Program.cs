08ee80de0d03d744d2c06cc1deed4940643079cb
Rifat Erdem Sahin <rifaterdemsahin@hotmail.com>
2018-03-07 15:27:59 +0000

    second commit

---
+++ AIF/InfrastruceAsCode/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace InfrastruceAsCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
