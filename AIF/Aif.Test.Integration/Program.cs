0afdc235e0b142d0845427f17ba80012c84c3e5c
Rifat Erdem Sahin <rifaterdemsahin@hotmail.com>
2018-03-17 10:05:57 +0000

    desktop code standing by...  adding new layers as far as I remember....Will look at serverless on GCP

---
+++ AIF/Aif.Test.Integration/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Aif.Test.Integration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
